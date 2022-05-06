fac31b46ff1786f10289f4dcada56dd1bd341587
BUDAHOOD <90276048+BUDAHOOD@users.noreply.github.com>
2022-05-06 10:25:32 -0400

    Update 3-solo-consonantes.tsx
    Se agrega validación por cada uno de los caracteres que pueda contener el input, no solo de la última letra.

--- src/componentes/3-solo-consonantes.tsx
+++ src/componentes/3-solo-consonantes.tsx
@@ -3,19 +3,22 @@ import { FC, useState, useEffect} from 'react'
 
 export const SoloConsonantes: FC = () => {
     const [palabra, setPalabra] = useState('')
+    //Expresión regular solo con consonantes
+    const r = /^[bcdfghjklmnñpqrstvwxyz]/;
     useEffect(() => {
-        //Se obtiene la última letra de la palabra ingresada
-        let letra = palabra.substring(palabra.length-1, palabra.length)
-        //Se considera la letra en minúscula solo para reducir el universo en la expresión regular
-        let min = letra.toLowerCase()
-        //Expresión regular para solo considerar las consonantes
-        const r = /^[bcdfghjklmnñpqrstvwxyz]/;
-        //Se aplica expresión regular, cualquier caracter distinto a la expresión regular se quita de la palabra 
-        if(!r.test(min)){
-            //Si no cumple con la expresión regular se quita la última letra ingresada
-            setPalabra(palabra.replace(letra,''))
+        //Por cada modificación en el input se recorre la palabra completa para validar los caracteres que se puedan ingresar        
+        for(let i = 0; i<palabra.length; i++){
+            let letra = palabra.substring(i, i+1)
+            //La letra obtenida se considera en minúscula para comparar con una expresión regular más acotada
+            let min = letra.toLowerCase()
+            //la letra se compara contra la expresión regular
+            if(!r.test(min)){
+                //Si la letra no se encuetra dentro se la expresión regular, se quita de la palabra reemplazándola por '' (vacío)
+                setPalabra(palabra.replace(letra,''))
+            }        
         }
     }, [palabra])
 
     return <><input autoFocus onChange={(e) => setPalabra(e.target.value)} type="text" id="txtConsonantes" name="txtConsonantes" value={palabra} /></>
 }
+  
